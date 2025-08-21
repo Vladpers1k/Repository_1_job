5062169b55d05fc77969f1f1fc16f06fe8eb7cb9
Vladpers1k <vladik340123@gmail.com>
2025-08-21 14:31:07 +0300

    vercel commit

--- apps/client/src/pages/UsersPage.tsx
+++ apps/client/src/pages/UsersPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import UserCard from "../components/UserCard";
 
-const API_URL = "http://localhost:3000";
+const API_URL = process.env.REACT_APP_API_URL as string;
 
 export default function UsersPage() {
   const [users, setUsers] = useState<any[]>([]);
